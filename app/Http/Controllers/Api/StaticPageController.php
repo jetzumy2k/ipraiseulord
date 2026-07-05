01b91a5dcb5de34c2f5e725301508ca1fcdb593e
july <july@webfitters.com>
2026-07-05 14:46:40 +0800

    master: messed page on donation.

--- app/Http/Controllers/Api/StaticPageController.php
+++ app/Http/Controllers/Api/StaticPageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\HandlesCrud;
 use App\Models\StaticPage;
 use App\Support\PageRoutes;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
